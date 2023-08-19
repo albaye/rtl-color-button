33af90fec627ce4450f387a429d0bee200656601
albaye <alba.ye99@gmail.com>
2023-08-19 16:43:46 +0100

    test button color, and click

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { logRoles } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('buttons has correct initial color', () => {
-  const { container } = render(<App />);
-  logRoles(container)
+test('button has correct initial color, and updates when clicked', () => {
+  render(<App />);
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole('button', { name: "Change to blue" });
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the backgroun color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  expect(colorButton).toHaveTextContent("Change to red");
 });
-
-// test('button has correct inital text', () => {
-//   screen.getByRole('button', { name: "Change to blue" });
-// })
-
-// test('button turns blue when clicked', () => {
-
-// })
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+
+  const buttonHandler = () => {
+    setButtonColor(newButtonColor)
+  }
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button style={{ backgroundColor: buttonColor }} onClick={buttonHandler}>Change to {newButtonColor}</button>
     </div>
   );
 }
